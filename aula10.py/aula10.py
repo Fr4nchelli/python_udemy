c8b648330cb2c44cc98db3a7988bcc1d8ed48970
Fr4nchelli <Viniciusfrancelli@gmail.com>
2024-10-10 23:28:14 -0300

    décima aula

---
+++ aula10.py/aula10.py
@@ -0,0 +1,21 @@
+# Operações condicionais
+# usadas para controlar melhor o fluxo do código, evitando erros e tornando ele mais fluido
+# if (se) - elif (se não se) - else (se não)
+entrada = input('Voce quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema')
+elif entrada == 'sair':
+    print('Você saiu do sistema')
+else:
+    print('Entrada invalida')
+    
+#  Operadores de Comparação
+""" 
+>   maior
+<   menor
+>=  maior ou igual
+<=  menor ou igual
+==  igual
+!=  diferente
+"""
