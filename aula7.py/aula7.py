aee0953958dc550138becca23ebd3f56156796ce
Fr4nchelli <Viniciusfrancelli@gmail.com>
2024-10-10 23:27:34 -0300

    oitava aula

---
+++ aula7.py/aula7.py
@@ -0,0 +1,11 @@
+# Exercicio de fixação
+# Calculo de IMC
+
+nome = 'Vinicius Francelli'
+altura = 1.80
+peso = 56
+imc = peso / (altura * 2)
+
+print(nome, "tem", altura, "de altura,")
+print('pesa', peso, 'quilos', 'e seu IMC é ')
+print(imc)
