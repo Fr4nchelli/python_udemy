7c57603c667458df16c38c6928abba8669e288da
Fr4nchelli <Viniciusfrancelli@gmail.com>
2024-10-10 23:25:03 -0300

    terceira aula

---
+++ aula3.py/aula3.py
@@ -0,0 +1,21 @@
+# tipos de dados
+
+# Strings, o que são?
+
+print('Aloha') # são textos dentro de aspas, simples ou duplas
+
+# Int e Float
+# int = numero inteiro, representa qualquer numero positivo ou negativo
+print(123456)
+print(0)
+print(-10)
+# float = ponto flutuante, representa qualquer numero pos ou neg com ponto flutuante
+print(1.25)
+print(0.0)
+print(-0.5)
+
+# Boolean ou Boleano
+# retorna sim ou não, verdadeiro ou falso, etc
+
+print(10 == 10)
+print(10 == 11)
