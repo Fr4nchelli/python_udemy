adb23f31117d97d4e9401d643861499b68de5978
Fr4nchelli <Viniciusfrancelli@gmail.com>
2024-10-10 23:25:56 -0300

    quarta aula

---
+++ aula4.py/aula4.py
@@ -0,0 +1,12 @@
+# Coerção de tipos  
+# é preciso para somar/comparar os tipos diferentes
+
+print('a' + 'b') # concatenação de str com str
+
+#print('1' + 2) # o programa exibira um erro dizendo que não é possivel concatenar str com int
+
+# para fazer a coerção de um tipo para outro colocamos o tipo e parenteses: int('exemplo')
+
+print(int('1') + 2) # convertendo a str para int e concatenando com o int
+
+print(str(11) + ' num')
