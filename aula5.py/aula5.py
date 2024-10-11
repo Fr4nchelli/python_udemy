fa3ea3f2faeaf1450686457a3869bb74007705db
Fr4nchelli <Viniciusfrancelli@gmail.com>
2024-10-10 23:26:21 -0300

    quinta aula

---
+++ aula5.py/aula5.py
@@ -0,0 +1,36 @@
+# Variaveis
+# variaveis são usadas para salvar algo na memória do computador
+# iniciar com letras minusculas
+# atrubuir nomes que façam sentido a variavel, porem simples
+# nome_variavel = expressao ; nome_completo = 'Vinicius Francelli' ; print(nome_completo) : Vinicius Francelli
+
+"""
+nome_completo = 'Vinicius Francelli'
+idade = 25
+soma = 2 + 2
+maior_de_idade = idade >= 18
+
+print(nome_completo)
+print(idade)
+print('é maior de idade? ' , maior_de_idade)
+print(soma)
+
+"""
+
+# Exercícios para fixação
+
+# Criar variaveis com as seguintes informações: nome, sobrenome, idade, ano de nascimento, é maior de idade?, altura em metros
+
+nome = 'Vinicius'
+sobrenome = 'Francelli'
+idade = 25
+ano_nascimento = 1999
+maior_de_idade = idade >= 18
+altura_metros = 1.79
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de Nascimento: ', ano_nascimento)
+print('É maior de idade? ', maior_de_idade)
+print('Altura em metros: ', altura_metros)
