c83a3ce4878966f1e22bafdb8a26195ad44d0afb
Fr4nchelli <Viniciusfrancelli@gmail.com>
2024-10-10 23:14:26 -0300

    primeira aula

---
+++ aula1.py/aula1.py
@@ -0,0 +1,9 @@
+  # isso é um comentário!
+  # Pode ser usado para explicar o que uma linha de código faz por exemplo
+ 
+print('123') #também pode ser usado na frente de um código :) 
+
+"""
+isso pode ser usado para comentar em multilinhas, porém não é um tipo de comentário!!!
+o interpretador ainda lê, somente ignora elas ;)
+"""
