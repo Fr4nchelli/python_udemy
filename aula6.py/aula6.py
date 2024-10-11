5849eef8a3c14ebe3cf2e098dc1a933d0e6bcfb1
Fr4nchelli <Viniciusfrancelli@gmail.com>
2024-10-10 23:26:40 -0300

    sexta aula

---
+++ aula6.py/aula6.py
@@ -0,0 +1,38 @@
+ # Operadores Aritiméticos
+
+# Soma +
+soma = 1 + 1 ; print("Soma: ", soma)
+
+# Subtração -
+subtracao = 1 - 1 ; print("Subtração: ", subtracao)
+
+# Multiplicação *
+multiplicacao = 2 * 1 ; print("Multiplicação: ", multiplicacao)
+
+# Divisão / => sempre retorna float
+divisao = 10 / 3 ; print("Divisão: ", divisao)
+
+# Divisão Inteira // => arredonda para int
+divisao_inteira = 10 / 3 ; print("Divisão Inteira: ", divisao_inteira)
+
+# Exponenciação **
+exponenciacao = 2 ** 10 ; print("Exponenciação: ", exponenciacao)
+
+# Módulo % => resto da divisão
+modulo = 55 % 2 ; print("Módulo: ", modulo)
+
+# Peculiaridades de operadores
+print('--------------------------------------')
+print("   Peculiaridades dos operadores")
+
+# Concatenação +
+concatenacao = 'a' + 'b' + 'c' ; print("Concatenação: ", concatenacao)
+
+# Repetição *
+repeticao = 'a' * 5 ; print("Repetição: ", repeticao)
+
+# Precedência dos operadores
+# 1. (n + n)
+# 2. **
+# 3. + / // %
+# 4. + -
