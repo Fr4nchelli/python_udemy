7e931c925aa7c5d899ef579f65d2b4f410616f05
Fr4nchelli <Viniciusfrancelli@gmail.com>
2024-10-11 00:10:32 -0300

    27º aula

---
+++ aula27.py/aula27.py
@@ -0,0 +1,30 @@
+"""
+Interpretador do Python
+
+python mod.py (executa o mod)
+python -u (unbuffered)
+python -m mod (lib mod como script)
+python -c 'cmd' (comando)
+python -i mod.py (iterativo com mod)
+
+The zen of Pyhton
+
+Bonito é melhor que feio
+Explicito é melhor que implicito
+Simples é melhor que complexo
+Complexo é melhor que complicado
+Plano é melhor que aglomerado
+Esparso é melhor que denso
+Legibilidade conta
+Casos especiais não são especiais o bastante para quebrar as regras.
+Embora a praticidade vença a pureza.
+Erros nunca devem passar silenciosamente.
+A menos que sejam explicitamente silenciados.
+Diante da ambiguidade, recuse a tentação de adivinhar.
+Deve haver um -- e só um -- modo obvio para fazer algo.
+Embora esse modo possa não ser obvio a primeira vista a menos que você seja holandes
+Agoar é melhor que nunca
+embora nunca frequentemente seja melhor que *exatamente* agora.
+Se a implementação é dificl de explicar, é uma má ideia.
+Namespaces são uma grande ideia -- vamos fazer mais dessas!!!
+"""
